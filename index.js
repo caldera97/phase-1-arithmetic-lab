a656d89bdd31ed936593e2485a37b37306fd613b
David Caldera <davidcaldera13@yahoo.com>
2022-02-22 11:59:26 -0600

    commit tree 2da94044a8410f921f2ee09d089531f9998b579e

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+const num3= 4;
+const num4 = 1;
+let mod = num3 / num4;
+let max = Math.max(1, 20);
